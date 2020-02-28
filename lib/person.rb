409413c6564c788da8e086a9905b7f4d07e193dc
kiran ram <kram@lions.molloy.edu>
2020-02-28 03:31:31 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+def name=name(person_name)
+    @name = person_name
+  end
+  
+  def name
+   @name
+ end
+ 
+ def job=job(job_name)
+    @job = job_name
+  end
+  
+  def job
+   @job
+ end
+ 
+ end
